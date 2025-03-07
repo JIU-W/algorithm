8e090422f3c367383b44928ff3851185bea4240e
JIU-W <2661727280@qq.com>
2025-03-08 00:14:51 +0800

    每日一练

--- LeetCode/src/com/itjn/leetCode75/sixHundredAndFortyThree.java
+++ LeetCode/src/com/itjn/leetCode75/sixHundredAndFortyThree.java
@@ -2,7 +2,7 @@ package com.itjn.leetCode75;
 
 public class sixHundredAndFortyThree {
     public static void main(String[] args) {
-        double maxAverage = findMaxAverage(new int[]{-1}, 1);
+        double maxAverage = findMaxAverage1(new int[]{0,4,0,3,2}, 1);
         System.out.println(maxAverage);
     }
 
@@ -17,11 +17,29 @@ public class sixHundredAndFortyThree {
         int maxSum = sum;
         for (int i = 1; i <= n - k; i++) {
             sum = sum - nums[i - 1] + nums[i + k - 1];
-            maxSum = Math.max(maxSum,sum);
+            maxSum = Math.max(maxSum, sum);
         }
         avg = (double) maxSum / (double) k;
         return avg;
     }
 
+    //二刷
+    public static double findMaxAverage1(int[] nums, int k) {
+        int max = 0;
+        int n = nums.length;
+        for (int i = 0; i < k; i++) {
+            max += nums[i];
+        }
+        int i = 0, j = i + k - 1;
+        int sum = max;
+        while (i < n - k) {
+            j++;
+            sum = sum + nums[j];
+            sum = sum - nums[i];
+            i++;
+            max = Math.max(max, sum);
+        }
+        return max / (double)k;
+    }
 
 }
