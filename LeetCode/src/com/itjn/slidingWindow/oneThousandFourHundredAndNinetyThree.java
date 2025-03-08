6b193a0ed92d3375ce37ebe5f633a7d21dc837ac
JIU-W <2661727280@qq.com>
2025-03-08 16:37:15 +0800

    每日一练

--- LeetCode/src/com/itjn/slidingWindow/oneThousandFourHundredAndNinetyThree.java
+++ LeetCode/src/com/itjn/slidingWindow/oneThousandFourHundredAndNinetyThree.java
@@ -2,7 +2,7 @@ package com.itjn.slidingWindow;
 
 public class oneThousandFourHundredAndNinetyThree {
     public static void main(String[] args) {
-        int i = longestSubarray(new int[]{1,1,1});
+        int i = longestSubarray1(new int[]{1, 1, 1});
         System.out.println(i);
     }
 
@@ -13,20 +13,34 @@ public class oneThousandFourHundredAndNinetyThree {
         int l = 0;
         int count = 0;
         for (int r = 0; r < nums.length; r++) {
-            if(nums[r] == 0){
+            if (nums[r] == 0) {
                 count++;
                 includedZero = 1;
             }
-            while(count > 1){
+            while (count > 1) {
                 count -= 1 - nums[l];
                 l++;
             }
             max = Math.max(max, r - l + 1);
         }
-
-        if(includedZero == 0)return nums.length - 1;
-        if(count == 1)return max - 1;
+        if (includedZero == 0) return nums.length - 1;
+        if (count == 1) return max - 1;
         return max;
+    }
+
+    //二刷
+    public static int longestSubarray1(int[] nums) {
+        int zeroCount = 0, max = 0, l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if(nums[r] == 0)zeroCount++;
+            while(zeroCount > 1){
+                zeroCount = zeroCount - (1 - nums[l]);
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+        if(zeroCount == 0)return nums.length - 1;
+        return max - 1;
     }
 
 
