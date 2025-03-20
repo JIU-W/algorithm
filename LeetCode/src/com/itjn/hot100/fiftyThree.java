eba5b14bfeadb57ced672fb47871b8d017af7813
JIU-W <2661727280@qq.com>
2025-03-20 22:54:51 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/fiftyThree.java
+++ LeetCode/src/com/itjn/hot100/fiftyThree.java
@@ -2,7 +2,7 @@ package com.itjn.hot100;
 
 public class fiftyThree {
     public static void main(String[] args) {
-        int i = maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        int i = maxSubArray2(new int[]{5,4,-1,7,8});
         System.out.println(i);
     }
 
@@ -25,11 +25,22 @@ public class fiftyThree {
     //分治(线段树)
     public static int maxSubArray1(int[] nums) {
 
-
-
         return 0;
     }
 
-
+    //二刷
+    public static int maxSubArray2(int[] nums) {
+        int max;
+        int[] f = new int[nums.length];
+        f[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            f[i] = Math.max(f[i - 1] + nums[i], nums[i]);
+        }
+        max = f[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(f[i], max);
+        }
+        return max;
+    }
 
 }
