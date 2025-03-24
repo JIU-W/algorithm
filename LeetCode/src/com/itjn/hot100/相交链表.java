4352b2babae3e4aff3c8db5ba58abddd3d119797
JIU-W <2661727280@qq.com>
2025-03-24 21:52:29 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/相交链表.java
+++ LeetCode/src/com/itjn/hot100/相交链表.java
@@ -1,15 +1,51 @@
 package com.itjn.hot100;
 
+import javax.xml.soap.Node;
+import java.util.HashSet;
+import java.util.Set;
+
 public class 相交链表 {
 
     public static void main(String[] args) {
 
     }
 
+    //方法一：哈希表
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+        while(temp != null){
+            set.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while(temp != null){
+            if(set.contains(temp)){
+                return temp;
+            }
+            temp = temp.next;
+        }
         return null;
     }
+
+    //
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+        while(temp != null){
+            set.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while(temp != null){
+            if(set.contains(temp)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
 
     public class ListNode {
         int val;
