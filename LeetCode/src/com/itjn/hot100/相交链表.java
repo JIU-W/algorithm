8a6ebd56d69fae5286aaef948630977aba46ea70
JIU-W <2661727280@qq.com>
2025-03-24 21:30:10 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/相交链表.java
@@ -0,0 +1,24 @@
+package com.itjn.hot100;
+
+public class 相交链表 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
