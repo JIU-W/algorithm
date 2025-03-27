175cdc1b307da50db713f6ec6a104b9d117e37b9
JIU-W <2661727280@qq.com>
2025-03-27 22:56:07 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/相交链表.java
+++ LeetCode/src/com/itjn/hot100/相交链表.java
@@ -28,22 +28,26 @@ public class 相交链表 {
         return null;
     }
 
-    //
+    //方法二：双指针
     public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
-        ListNode temp = headA;
-        while(temp != null){
-            set.add(temp);
-            temp = temp.next;
+        if (headA == null || headB == null) {
+            return null;
         }
-        temp = headB;
-        while(temp != null){
-            if(set.contains(temp)){
-                return temp;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        while(tempA != tempB){
+            if(tempA == null){
+                tempA = headB;
+            }else{
+                tempA = tempA.next;
             }
-            temp = temp.next;
+            if(tempB == null){
+                tempB = headA;
+            }else{
+                tempB = tempB.next;
+            }
         }
-        return null;
+        return tempA;
     }
 
 
