044edd4b4bd5b7ef225fb63d64ecbc3e6b8dce23
JIU-W <2661727280@qq.com>
2025-03-27 22:22:47 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/螺旋矩阵.java
@@ -0,0 +1,19 @@
+package com.itjn.hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 螺旋矩阵 {
+    public static void main(String[] args) {
+        spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+
+
+        return list;
+    }
+
+}
