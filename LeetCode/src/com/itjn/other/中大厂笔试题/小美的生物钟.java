1512110a816ebd15a60c0243451f86a4bb96d0da
JIU-W <2661727280@qq.com>
2025-03-29 22:53:09 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/other/中大厂笔试题/小美的生物钟.java
@@ -0,0 +1,30 @@
+package com.itjn.other.中大厂笔试题;
+
+import java.util.Scanner;
+
+public class 小美的生物钟 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt(), k = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            }
+            if(sum <= n * k){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+
+            t--;
+        }
+
+    }
+
+}
