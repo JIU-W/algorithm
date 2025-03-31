b0add5c277cb293f969358824cc91dc86baf8370
JIU-W <2661727280@qq.com>
2025-03-31 19:26:30 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/反转链表.java
+++ LeetCode/src/com/itjn/hot100/反转链表.java
@@ -1,17 +1,26 @@
 package com.itjn.hot100;
 
+import java.util.Scanner;
+
 public class 反转链表 {
     public static void main(String[] args) {
-
-
+        Scanner sc = new Scanner(System.in);
+        ListNode ln = new ListNode(sc.nextInt());
+        ListNode head = ln;
+        while (!sc.hasNextInt()){
+            ListNode temp = ln;
+            temp.next = new ListNode(sc.nextInt());
+            ln = temp;
+        }
+        reverseList(head);
     }
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
 
         return null;
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
