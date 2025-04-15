a87ddc4fd497525e9f06dc44b01bd00f3181fad7
JIU-W <2661727280@qq.com>
2025-04-15 14:06:26 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/两数相加.java
+++ LeetCode/src/com/itjn/hot100/两数相加.java
@@ -2,33 +2,94 @@ package com.itjn.hot100;
 
 public class 两数相加 {
     public static void main(String[] args) {
+        ListNode node3 = new ListNode(1, null);
+        ListNode node2 = new ListNode(9, node3);
+        ListNode node1 = new ListNode(9, node2);
+
+        ListNode node4 = new ListNode(1, null);
+        ListNode list = addTwoNumbers(node1, node4);
+
+        for (ListNode ll = list; ll != null; ll = ll.next) {
+            System.out.println(ll.val);
+        }
 
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1.next == null && l1.val == 0){
-            return l2;
+    //模拟相加以及进位
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+
+        ListNode pre = new ListNode(-1);
+        ListNode res = pre;
+        int temp = 0;
+        while (temp1 != null && temp2 != null) {
+            int sum = temp1.val + temp2.val;
+            if (temp + sum < 10) {
+                res.next = new ListNode(sum + temp);
+                temp = 0;
+            } else {
+                res.next = new ListNode((temp + sum) % 10);
+                temp = (temp + sum) / 10;
+            }
+
+            res = res.next;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
         }
-        if (l2.next == null && l2.val == 0){
-            return l1;
+
+        if (temp1 == null && temp2 == null) {
+            if (temp != 0) {//temp为1
+                res.next = new ListNode(temp);
+            }
+            return pre.next;
         }
 
+        if (temp1 == null) {
+            while (temp2 != null) {
+                if (temp + temp2.val < 10) {
+                    res.next = new ListNode(temp2.val + temp);
+                    temp = 0;
+                } else {
+                    res.next = new ListNode((temp + temp2.val) % 10);
+                    temp = (temp + temp2.val) / 10;
+                }
+                res = res.next;
+                temp2 = temp2.next;
+            }
+        } else {
+            while (temp1 != null) {
+                if (temp + temp1.val < 10) {
+                    res.next = new ListNode(temp1.val + temp);
+                    temp = 0;
+                } else {
+                    res.next = new ListNode((temp + temp1.val) % 10);
+                    temp = (temp + temp1.val) / 10;
+                }
+                res = res.next;
+                temp1 = temp1.next;
+            }
+        }
 
+        if (temp != 0) {//temp为1
+            res.next = new ListNode(temp);
+        }
 
-
-
-
-        return null;
+        return pre.next;
     }
 
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
+
+        ListNode() {
+        }
+
         ListNode(int val) {
             this.val = val;
         }
+
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
