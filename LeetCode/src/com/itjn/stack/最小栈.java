e07d4e77fde8057c1015fb6a3985d5c19439bc79
JIU-W <2661727280@qq.com>
2025-04-17 00:04:07 +0800

    每日一练

--- LeetCode/src/com/itjn/stack/最小栈.java
+++ LeetCode/src/com/itjn/stack/最小栈.java
@@ -1,11 +1,38 @@
 package com.itjn.stack;
 
+import com.itjn.hot100.删除链表的倒数第N个结点;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
 public class 最小栈 {
-    public static void main(String[] args) {
 
+    class MinStack {
+        Deque<Integer> stack;
+        Deque<Integer> minStack;
 
+        public MinStack() {
+            stack = new ArrayDeque<>();
+            minStack = new ArrayDeque<>();
+            minStack.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            minStack.push(Math.min(minStack.peek(), val));
+        }
+        public void pop() {
+            stack.pop();
+            minStack.pop();
+        }
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
     }
-
-
 
 }
