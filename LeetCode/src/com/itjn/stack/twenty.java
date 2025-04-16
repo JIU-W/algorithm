b59eba31aeb03766803c9dcb3dfccc342e1b9d1f
JIU-W <2661727280@qq.com>
2025-04-16 23:43:25 +0800

    每日一练

--- LeetCode/src/com/itjn/stack/twenty.java
+++ LeetCode/src/com/itjn/stack/twenty.java
@@ -1,8 +1,6 @@
 package com.itjn.stack;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class twenty {
     public static void main(String[] args) {
@@ -13,20 +11,20 @@ public class twenty {
     //方法一：栈 + 哈希
     public static boolean isValid(String s) {
         int n = s.length();
-        if(n % 2 == 1)return false;
+        if (n % 2 == 1) return false;
 
         Map<Character, Character> map = new HashMap<>();
-        map.put('(',')');
-        map.put('[',']');
-        map.put('{','}');
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
-            if(map.containsKey(s.charAt(i))){
+            if (map.containsKey(s.charAt(i))) {
                 stack.push(s.charAt(i));
-            }else{
-                if(stack.empty())return false;
-                if(map.get(stack.peek()) != s.charAt(i)){
+            } else {
+                if (stack.empty()) return false;
+                if (map.get(stack.peek()) != s.charAt(i)) {
                     return false;
                 }
                 stack.pop();
@@ -37,21 +35,54 @@ public class twenty {
 
     //方法二：
     public boolean isValid1(String s) {
-        if(s.length() % 2 != 0) return false;
+        if (s.length() % 2 != 0) return false;
         char[] array = s.toCharArray();
         int idx = 0;
-        for(char c : array){
-            if(c == '('){
-                array[idx ++] = ')';
-            }else if(c == '['){
-                array[idx ++] = ']';
-            }else if(c == '{'){
-                array[idx ++] = '}';
-            }else if(idx == 0 || array[-- idx] != c){
+        for (char c : array) {
+            if (c == '(') {
+                array[idx++] = ')';
+            } else if (c == '[') {
+                array[idx++] = ']';
+            } else if (c == '{') {
+                array[idx++] = '}';
+            } else if (idx == 0 || array[--idx] != c) {
                 return false;
             }
         }
         return idx == 0;
+    }
+
+    //二刷
+    public static boolean isValid2(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (c == ')') {
+                    if(stack.peek() == null || stack.peek() != '('){
+                        return false;
+                    }else{
+                        stack.pop();
+                    }
+                } else if (c == ']') {
+                    if(stack.peek() == null || stack.peek() != '['){
+                        return false;
+                    }else{
+                        stack.pop();
+                    }
+                } else {
+                    if(stack.peek() == null || stack.peek() != '{'){
+                        return false;
+                    }else{
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        if(stack.peek() != null)return false;
+        return true;
     }
 
 }
---
+++ LeetCode/src/com/itjn/stack/最小栈.java
@@ -0,0 +1,11 @@
+package com.itjn.stack;
+
+public class 最小栈 {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
