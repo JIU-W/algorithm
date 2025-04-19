902a89b4313e0d8f659637694cdb83fcae30e083
JIU-W <2661727280@qq.com>
2025-04-19 17:53:48 +0800

    每日一练

--- LeetCode/src/com/itjn/other/中大厂笔试题/蚂蚁/圈圈字符串.java
+++ LeetCode/src/com/itjn/other/中大厂笔试题/蚂蚁/圈圈字符串.java
@@ -6,14 +6,22 @@ public class 圈圈字符串 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int count = 0;
+        int sum = 0;
+        int count = 0, noCount = 0;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (c == 'a' || c == 'b' || c == 'd' || c == 'e' || c == 'g' || c == 'o' || c == 'p' || c == 'q') {
                 count++;
+            }else{
+                noCount++;
+            }
+            if(count > noCount){
+                sum++;
+                count = 0;
+                noCount = 0;
             }
         }
-        System.out.println(count);
+        System.out.println(sum);
     }
 
 }
---
+++ LeetCode/src/com/itjn/other/中大厂笔试题/蚂蚁/小苯的数组.java
@@ -0,0 +1,8 @@
+package com.itjn.other.中大厂笔试题.蚂蚁;
+
+public class 小苯的数组 {
+    public static void main(String[] args) {
+
+
+    }
+}
