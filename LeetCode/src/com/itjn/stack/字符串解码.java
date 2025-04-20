b47c555fcfff5e368655c8ff3e902bf9cc260cca
JIU-W <2661727280@qq.com>
2025-04-21 00:11:50 +0800

    每日一练

--- LeetCode/src/com/itjn/stack/字符串解码.java
+++ LeetCode/src/com/itjn/stack/字符串解码.java
@@ -1,22 +1,78 @@
 package com.itjn.stack;
 
+import javax.xml.stream.events.Characters;
+import java.util.*;
+
+
 public class 字符串解码 {
     public static void main(String[] args) {
-        decodeString("");
+        System.out.println(decodeString("3[a2[c]]"));
     }
 
 
-    //方法一：辅助栈法
+    //方法一：栈操作(这里要用链表模拟栈，更为方便)
+    static int ptr;
     public static String decodeString(String s) {
+        LinkedList<String> stk = new LinkedList<String>();
+        ptr = 0;
+        while (ptr < s.length()) {
+            char cur = s.charAt(ptr);
+            if (Character.isDigit(cur)) {
+                // 获取一个数字并进栈
+                String digits = getDigits(s);
+                stk.addLast(digits);
+            } else if (Character.isLetter(cur) || cur == '[') {
+                // 获取一个字母并进栈
+                stk.addLast(String.valueOf(s.charAt(ptr++)));
+            } else {
+                ++ptr;
+                LinkedList<String> sub = new LinkedList<String>();
+                while (!"[".equals(stk.peekLast())) {
+                    sub.addLast(stk.removeLast());//注：只有LinkedList有addLast()方法，ArrayList和List都没有
+                }
+                Collections.reverse(sub);
+                // 左括号出栈
+                stk.removeLast();
+                // 此时栈顶为当前 sub 对应的字符串应该出现的次数
+                int repTime = Integer.parseInt(stk.removeLast());
+                StringBuffer t = new StringBuffer();
+                String o = getString(sub);
+                // 构造字符串
+                while (repTime-- > 0) {
+                    t.append(o);
+                }
+                // 将构造好的字符串入栈
+                stk.addLast(t.toString());
+            }
+        }
 
-
-        return null;
+        return getString(stk);
     }
 
-    //方法二：递归
+    public static String getDigits(String s) {
+        StringBuffer ret = new StringBuffer();
+        while (Character.isDigit(s.charAt(ptr))) {
+            ret.append(s.charAt(ptr++));
+        }
+        return ret.toString();
+    }
+
+    //链表集合转成字符串
+    public static String getString(LinkedList<String> list) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+        }
+        return sb.toString();
+    }
+
+
+
+    //方法二：递归(不需要会这种方法，会一种方法(栈模拟)就行)
     public static String decodeString1(String s) {
 
-
         return null;
     }
+
+
 }
