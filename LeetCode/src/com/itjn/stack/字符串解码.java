3c41494e426ea11ec8639d9c6130b27b7e1cd093
JIU-W <2661727280@qq.com>
2025-04-20 18:54:56 +0800

    每日一练

--- LeetCode/src/com/itjn/stack/字符串解码.java
+++ LeetCode/src/com/itjn/stack/字符串解码.java
@@ -6,10 +6,17 @@ public class 字符串解码 {
     }
 
 
+    //方法一：辅助栈法
     public static String decodeString(String s) {
 
-        return "";
+
+        return null;
     }
 
+    //方法二：递归
+    public static String decodeString1(String s) {
 
+
+        return null;
+    }
 }
