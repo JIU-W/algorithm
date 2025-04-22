04d60e65f625998ca5783b812ac2abef16396bd3
JIU-W <2661727280@qq.com>
2025-04-22 14:16:31 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/二叉树的最大深度.java
@@ -0,0 +1,30 @@
+package com.itjn.hot100;
+
+public class 二叉树的最大深度 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int maxDepth(TreeNode root) {
+
+        return 0;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
