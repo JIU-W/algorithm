34a7dc89cc8770e9ab7183e1be53787d1dde74d5
JIU-W <2661727280@qq.com>
2025-04-24 10:19:48 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/二叉树的层序遍历.java
@@ -0,0 +1,56 @@
+package com.itjn.hot100;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 二叉树的层序遍历 {
+    public static void main(String[] args) {
+
+
+    }
+
+    //广度优先遍历
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size;
+        while(!queue.isEmpty()){
+            size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while(size != 0){
+                TreeNode head = queue.poll();
+                temp.add(head.val);
+                if(head.left != null){
+                    queue.offer(head.left);
+                }
+                if(head.right != null){
+                    queue.offer(head.right);
+                }
+                size--;
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
