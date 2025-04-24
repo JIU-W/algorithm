138942593a28c58187a2254e80330c055763795b
JIU-W <2661727280@qq.com>
2025-04-24 10:32:43 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/将有序数组转换为二叉搜索树.java
@@ -0,0 +1,29 @@
+package com.itjn.hot100;
+
+public class 将有序数组转换为二叉搜索树 {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
