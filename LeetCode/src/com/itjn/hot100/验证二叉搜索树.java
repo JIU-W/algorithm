316b4ea2d0e641912b4533cca63f54156629d2ae
JIU-W <2661727280@qq.com>
2025-04-27 15:25:44 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/验证二叉搜索树.java
+++ LeetCode/src/com/itjn/hot100/验证二叉搜索树.java
@@ -1,6 +1,11 @@
 package com.itjn.hot100;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class 验证二叉搜索树 {
+
+    //递归
     public boolean isValidBST(TreeNode root) {
         return isBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
@@ -15,7 +20,24 @@ public class 验证二叉搜索树 {
         return isBST(node.left, low, node.val) && isBST(node.right, node.val, upper);
     }
 
-
+    //栈模拟
+    public boolean isValidBST2(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        long temp = Long.MIN_VALUE;
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode pop = stack.pop();
+            if(pop.val <= temp){
+                return false;
+            }
+            temp = pop.val;
+            root = pop.right;
+        }
+        return true;
+    }
 
     public class TreeNode {
         int val;
