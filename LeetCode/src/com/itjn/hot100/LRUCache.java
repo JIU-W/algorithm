5bc14a266dd1a1fd13e383e90164fe21ed0c5696
JIU-W <2661727280@qq.com>
2025-04-27 22:15:45 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/LRUCache.java
+++ LeetCode/src/com/itjn/hot100/LRUCache.java
@@ -2,7 +2,12 @@ package com.itjn.hot100;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * @description 哈希表 + 双向链表
+ * @author JIU-W
+ * @date 2025-04-27
+ * @version 1.0
+ */
 public class LRUCache {
 
     class DLinkedNode {
@@ -14,16 +19,18 @@ public class LRUCache {
         public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
     }
 
-    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
     private int size;
     private int capacity;
+
+    //伪头部节点和伪尾部节点
     private DLinkedNode head, tail;
 
 
     public LRUCache(int capacity) {
         this.size = 0;
         this.capacity = capacity;
-        // 使用伪头部和伪尾部节点
+        //使用伪头部和伪尾部节点
         head = new DLinkedNode();
         tail = new DLinkedNode();
         head.next = tail;
@@ -31,40 +38,44 @@ public class LRUCache {
     }
 
     public int get(int key) {
+        //先通过哈希表定位
         DLinkedNode node = cache.get(key);
         if (node == null) {
             return -1;
         }
-        // 如果 key 存在，先通过哈希表定位，再移到头部
+        //key存在，移到头部
         moveToHead(node);
+        //返回结果
         return node.value;
     }
 
     public void put(int key, int value) {
         DLinkedNode node = cache.get(key);
         if (node == null) {
-            // 如果 key 不存在，创建一个新的节点
+            //如果 key 不存在，创建一个新的节点
             DLinkedNode newNode = new DLinkedNode(key, value);
-            // 添加进哈希表
+            //添加进哈希表
             cache.put(key, newNode);
-            // 添加至双向链表的头部
+            //添加至双向链表的头部
             addToHead(newNode);
-            ++size;
+            size++;
             if (size > capacity) {
-                // 如果超出容量，删除双向链表的尾部节点
+                //如果超出容量，删除双向链表的尾部节点
                 DLinkedNode tail = removeTail();
-                // 删除哈希表中对应的项
+                //删除哈希表中对应的项
                 cache.remove(tail.key);
-                --size;
+                size--;
             }
         }
         else {
-            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+            //如果key存在，先通过哈希表定位，再修改value并移到头部
             node.value = value;
+            //移到头部
             moveToHead(node);
         }
     }
 
+    //添加至双向链表的头部
     private void addToHead(DLinkedNode node) {
         node.prev = head;
         node.next = head.next;
@@ -72,20 +83,27 @@ public class LRUCache {
         head.next = node;
     }
 
-    private void removeNode(DLinkedNode node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
+    //将节点移到头部
     private void moveToHead(DLinkedNode node) {
+        //1.先移除当前操作的节点
         removeNode(node);
+        //2.再将当前操作的节点添加到双向链表的头部
         addToHead(node);
     }
 
+    //删除双向链表的尾部节点
     private DLinkedNode removeTail() {
+        //res是尾节点
         DLinkedNode res = tail.prev;
+        //移除节点(尾节点)
         removeNode(res);
         return res;
+    }
+
+    //移除节点
+    private void removeNode(DLinkedNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
 
 }
