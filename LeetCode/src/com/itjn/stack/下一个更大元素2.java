edfd23d37b74d63cedcce8eecf4d45a6574b83b0
JIU-W <2661727280@qq.com>
2025-04-28 20:42:39 +0800

    每日一练

--- LeetCode/src/com/itjn/stack/下一个更大元素2.java
+++ LeetCode/src/com/itjn/stack/下一个更大元素2.java
@@ -1,19 +1,36 @@
 package com.itjn.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class 下一个更大元素2 {
     public static void main(String[] args) {
 
 
     }
 
+    //单调栈 + 循环数组
     public int[] nextGreaterElements(int[] nums) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             max = Math.max(max, nums[i]);
         }
         int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == max)res[i] = -1;
+        }
+        int n = nums.length;
+        Deque<Integer> stack = new ArrayDeque<>();
 
-
-        return null;
+        //二次遍历数组
+        for (int i = 0; i < 2 * n - 1; i++) {
+            while(!stack.isEmpty() && nums[i % n] > nums[stack.peek()]){
+                Integer pop = stack.pop();
+                res[pop] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return res;
     }
+
 }
