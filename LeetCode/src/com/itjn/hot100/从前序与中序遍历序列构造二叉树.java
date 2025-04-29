98cd52cf75a9f6cadd57416f36b7003cad212f8a
JIU-W <2661727280@qq.com>
2025-04-29 22:56:10 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/hot100/从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,30 @@
+package com.itjn.hot100;
+
+public class 从前序与中序遍历序列构造二叉树 {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+        return null;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
