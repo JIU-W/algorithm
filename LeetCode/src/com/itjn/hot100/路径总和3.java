c4dd9533fb761d7839959f0d649c07f2997605f8
JIU-W <2661727280@qq.com>
2025-04-30 17:31:44 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/路径总和3.java
+++ LeetCode/src/com/itjn/hot100/路径总和3.java
@@ -5,4 +5,45 @@ public class 路径总和3 {
 
 
     }
+
+    //深度优先搜索(递归)
+    public int pathSum(TreeNode root, long targetSum) {
+        if(root == null){
+            return 0;
+        }
+        int sum = rootSum(root, targetSum);
+        sum += pathSum(root.left, targetSum);
+        sum += pathSum(root.right, targetSum);
+        return sum;
+    }
+
+    private int rootSum(TreeNode root, long targetSum) {
+        if(root == null){
+            return 0;
+        }
+        int ret = 0;
+        if(root.val == targetSum){
+            ret++;
+        }
+        ret += rootSum(root.left, targetSum- root.val);
+        ret += rootSum(root.right, targetSum- root.val);
+        return ret;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
 }
