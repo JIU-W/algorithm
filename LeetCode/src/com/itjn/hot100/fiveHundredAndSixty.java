bed81b00b5f41a0e34059ee2e82da84eaa5fef69
JIU-W <2661727280@qq.com>
2025-05-09 12:20:42 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/fiveHundredAndSixty.java
+++ LeetCode/src/com/itjn/hot100/fiveHundredAndSixty.java
@@ -76,8 +76,20 @@ public class fiveHundredAndSixty {
         return count;
     }
 
-
-
+    //三刷
+    public static int subarraySum4(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int preSum = 0,count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            preSum += nums[i];
+            if(map.containsKey(preSum - k)){
+                count += map.get(preSum - k);
+            }
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
 
 
 
