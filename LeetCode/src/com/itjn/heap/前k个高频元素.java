90fd0b9c06b893aa93fb6422fc44ab32fa77508c
JIU-W <2661727280@qq.com>
2025-05-10 16:53:11 +0800

    每日一练

---
+++ LeetCode/src/com/itjn/heap/前k个高频元素.java
@@ -0,0 +1,13 @@
+package com.itjn.heap;
+
+public class 前k个高频元素 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        return null;
+    }
+
+}
--- LeetCode/src/com/itjn/heap/数组中的第k个最大元素.java
+++ LeetCode/src/com/itjn/heap/数组中的第k个最大元素.java
@@ -1,5 +1,7 @@
 package com.itjn.heap;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -9,14 +11,9 @@ public class 数组中的第k个最大元素 {
         System.out.println(kthLargest);
     }
 
-
+    //优先队列(堆)：基于堆排序的选择方法
     public static int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2 - o1);
         for (int i = 0; i < nums.length; i++) {
             queue.offer(nums[i]);
         }
@@ -24,6 +21,27 @@ public class 数组中的第k个最大元素 {
             queue.poll();
         }
         return queue.peek();
+    }
+
+    //快速排序
+    public static int findKthLargest1(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    //计数排序/桶排序
+    public static int findKthLargest2(int[] nums, int k) {
+        int[] buckets = new int[20001];
+        for (int i = 0; i < nums.length; i++) {
+            buckets[nums[i] + 10000]++;
+        }
+        for (int i = 20000; i >= 0; i--) {
+            k = k - buckets[i];
+            if(k <= 0){
+                return i - 10000;
+            }
+        }
+        return 0;
     }
 
 }
