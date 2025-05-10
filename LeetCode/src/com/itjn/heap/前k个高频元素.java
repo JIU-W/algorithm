23732513ca6307cb0eb323fb2ab73afdb132079a
JIU-W <2661727280@qq.com>
2025-05-10 19:38:38 +0800

    每日一练

--- LeetCode/src/com/itjn/heap/前k个高频元素.java
+++ LeetCode/src/com/itjn/heap/前k个高频元素.java
@@ -1,13 +1,38 @@
 package com.itjn.heap;
 
+import java.util.*;
+
 public class 前k个高频元素 {
     public static void main(String[] args) {
 
     }
 
+    //堆(优先队列)
     public int[] topKFrequent(int[] nums, int k) {
-
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        //先用map集合计数
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        //根据计数"倒序排序"
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[1] - o1[1];
+            }
+        });
+        //把"数据"和"数据对应的计数"放入到优先队列中去，且按计数倒序排序。
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            Integer key = entry.getKey();
+            Integer count = entry.getValue();
+            heap.offer(new int[]{key, count});
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[0];
+        }
+        return res;
     }
 
 }
