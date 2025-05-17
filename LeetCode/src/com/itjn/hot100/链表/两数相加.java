25fbb6dd98f5c4f597a39995b8ddc839bdda59f4
JIU-W <2661727280@qq.com>
2025-05-17 15:14:49 +0800

    每日一练

--- LeetCode/src/com/itjn/hot100/链表/两数相加.java
+++ LeetCode/src/com/itjn/hot100/链表/两数相加.java
@@ -78,6 +78,66 @@ public class 两数相加 {
         return pre.next;
     }
 
+    //二刷
+    public static ListNode addTwoNumbers1(ListNode l1, ListNode l2){
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+
+        ListNode pre = new ListNode();
+        ListNode res = pre;
+        int temp = 0;
+        while(temp1 != null && temp2 != null){
+            int v = temp1.val + temp2.val;
+            if(v + temp < 10){
+                res.next = new ListNode(v + temp);
+                temp = 0;
+            }else{
+                res.next = new ListNode((v + temp) % 10);
+                temp = 1;
+            }
+            res = res.next;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        if(temp1 == null && temp2 == null){
+            if(temp != 0){
+                res.next = new ListNode(temp);
+            }
+            return pre.next;
+        }
+
+        if(temp1 == null){
+            while(temp2 != null){
+                if(temp2.val + temp < 10){
+                    res.next = new ListNode(temp2.val + temp);
+                    temp = 0;
+                }else{
+                    res.next = new ListNode((temp2.val + temp) % 10);
+                    temp = 1;
+                }
+                temp2 = temp2.next;
+                res = res.next;
+            }
+        }else{
+            while(temp1 != null){
+                if(temp1.val + temp < 10){
+                    res.next = new ListNode(temp1.val + temp);
+                    temp = 0;
+                }else{
+                    res.next = new ListNode((temp1.val + temp) % 10);
+                    temp = 1;
+                }
+                temp1 = temp1.next;
+                res = res.next;
+            }
+        }
+
+        if(temp != 0){
+            res.next = new ListNode(temp);
+        }
+        return pre.next;
+    }
 
     public static class ListNode {
         int val;
